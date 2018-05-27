20a92984de7d2fa8e33dde8463978c094160d925
nicolashidalgo1989 <nicolas.hidalgo.1989@gmail.com>
2018-05-27 18:46:46 -0400

    functions

--- app.js
+++ app.js
@@ -1,32 +1,60 @@
 const calculator = document.querySelector('.calculator');
 const display = calculator.querySelector('.calculator__display');
-const keys = calculator.querySelector('.calculator__keys');
+const keys = calculator.querySelector('.calculator__keys'); 
 keys.addEventListener('click', clickButton);
 function clickButton(e){
-    if (e.target.matches('button')) {
-        let number = 0 ;
-        let key = e.target;
-        let action = key.dataset.action;
+    if (e.target.matches('button')) { 
+        const key = e.target;
+        const action = key.dataset.action;
+        const nKey = key.textContent;
+        const nDisplay = display.textContent;
+        let previousKeyType = calculator.dataset.previousKeyType;
         if(!action) {
-            console.log('number key!')
-            if(display.innerHTML==='0'){
-                display.innerHTML = key.innerHTML; 
-            }else{
-                key.innerHTML.appendChild(display);
+            console.log('number key!');
+            if( nDisplay === '0' || previousKeyType === 'operator'){
+                display.textContent = nKey; 
+            } else{ 
+                display.textContent = nDisplay + nKey;
             }
         } 
         if(action === 'add' || action === 'subtract' || action === 'multiply' ||  action === 'divide') 
         {
-            console.log('operator key!')
+            console.log('operator key!'); 
+            key.classList.add('is-depressed');
+            calculator.dataset.previousKeyType = 'operator'; 
+            calculator.dataset.firstValue = nDisplay;
+            calculator.dataset.operator = action;
         }
         if(action === 'decimal'){
             console.log('decimal key!')
+            display.textContent = nDisplay + nKey;
         }
         if(action === 'clear'){
-            console.log('clear key!')
+            console.log('clear key!');
+            display.textContent = 0;  
         }
         if(action === 'calculate'){
-            console.log('equal key!')
+            console.log('equal key!');
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = nDisplay;
+            display.textContent = calculate(firstValue, operator, secondValue);
+            function calculate(n1, operator, n2){ 
+                let result = '';
+                if(operator==='add'){
+                    result = parseFloat(n1) + parseFloat(n2);
+                }else if(operator==='subtract'){
+                    result = n1 - n2;
+                }else if(operator==='multiply'){
+                    result = n1 * n2;
+                }else if(operator==='divide'){
+                    result = n1 / n2;
+                } 
+                return result;
+                console.log(result);
+            }
         }
+        // Remove .is-depressed class from all keys
+        Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed')) 
     }
 }
